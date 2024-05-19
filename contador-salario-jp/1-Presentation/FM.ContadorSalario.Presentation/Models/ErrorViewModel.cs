ec8bed987bec916ab977b2b87fe9b5cccc6bb8d7
masanori-iha <masanori.iha@outlook.com>
2024-05-19 01:23:35 -0300

    first commit

---
+++ contador-salario-jp/1-Presentation/FM.ContadorSalario.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FM.ContadorSalario.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
